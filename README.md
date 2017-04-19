fd48798af41189474bfe4466fc9a4bd6d6130bf4
demba003 <kd.debinski@gmail.com>
2017-04-19 10:18:37 +0200

    update
    graph api and caching

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # FB messages counter
 
 Very simple tool that analyzes messages.htm file containg whole messaging history made on facebook (you can download yours here: https://www.facebook.com/settings).
-At the moment it's just a proof of concept
+To get friends' names it uses Facebook Graph API, you can get your access token here: https://developers.facebook.com/tools/explorer/
 
 ## Features
 + Count threads
-+ Count messages in every thread and display participants (when unable to fetch name (e.g. someone's profile is private or deleted) displays link that may open properly if you are logged in)
++ Count messages in every thread and display participants. When unable to fetch name (someone's profile is deleted) displays link
++ Added caching friends' names so it's not necessary do download them every time (works much faster)
++ Added some support for Facebook Graph API so now it's required to paste access token during first launch (no more security check errors)
 
 ## Instructions
 + Place fbparser.jar and messages.htm in the same directory
 + Open command line inside the chosen directory, write ```java -jar fbparser.jar``` and hit Enter
 + If you don't have internet connection or block java on firewall you'll get only links to profiles
++ On first launch you have to paste access token, it will be stored in the "tokenfile" in the same directory, friends' names will be cached in the "friends" file
--- src/main/java/com/demba/fbparser/App.java
+++ src/main/java/com/demba/fbparser/App.java
@@ -1,56 +1,123 @@
 package com.demba.fbparser;
 
-import org.jsoup.Connection;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.IOException;
+import org.json.*;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
+    private static String token = "";
+
     public static void main(String[] args) {
 
-        Document doc;
         try {
-            File input = new File("messages.htm");
-            doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
-
-            // get all links
-            Elements threads = doc.select("div.thread");
-
-            System.out.println("Threads count: " + threads.size());
-
-            int i=0;
-            for (Element th : threads) {
-                System.out.println("-------------------------");
-                Elements mes = th.select("div.message");
-
-                String threadString = th.toString();
-                String people = threadString.substring(22, threadString.indexOf("<div class=\"message\">") - 2);
-
-                String[] peopleArray = people.split(", ");
-
-                for(String person : peopleArray){
-                    String personURL = "https://www.facebook.com/profile.php?id=" + person.substring(0, person.indexOf("@"));
-                    try {
-                        Connection connect = Jsoup.connect(personURL);
-                        System.out.println(connect.get().select("span#fb-timeline-cover-name").text());
-                    } catch (Exception e) {
-                        System.out.println(personURL);
-                    }
-                }
-                System.out.println("");
-
-                System.out.println("Messages count: " + mes.size());
-                i++;
+           token = Files.readAllLines(Paths.get("tokenfile")).get(0);
+        } catch (NoSuchFileException e) {
+            System.out.println("Get your Facebook Graph API token: https://developers.facebook.com/tools/explorer/");
+            System.out.println("And paste it here:");
+            Scanner scanner = new Scanner(System.in);
+            token = scanner.nextLine();
+            List<String> lines = Arrays.asList(token);
+            try {
+                Files.write(Paths.get("tokenfile"), lines, Charset.forName("UTF-8"));
+            } catch (Exception ex) {
+                System.out.println(ex);
+                System.exit(0);
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println(ex);
+            System.exit(0);
         }
 
+        try {
+            PropertiesConfiguration friends = new PropertiesConfiguration("friends");
+            Document doc;
+
+            try {
+                File input = new File("messages.htm");
+                doc = Jsoup.parse(input, "UTF-8");
+
+                Elements threads = doc.select("div.thread");
+
+                System.out.println("Threads count: " + threads.size());
+
+                int i=0;
+                for (Element th : threads) {
+                    System.out.println("-------------------------");
+                    Elements mes = th.select("div.message");
+
+                    String threadString = th.toString();
+                    String people = threadString.substring(22, threadString.indexOf("<div class=\"message\">") - 2);
+
+                    String[] peopleArray = people.split(", ");
+
+                    for(String person : peopleArray){
+
+                        String personId = person.substring(0, person.indexOf("@"));
+                        String name;
+
+                        try {
+                            if (null == friends.getProperty(personId)) {
+                                JSONObject json = readJsonFromUrl("https://graph.facebook.com/v2.9/" + personId + "?fields=name&access_token=" + token);
+                                name = json.get("name").toString();
+                                System.out.println(name);
+                                friends.setProperty(personId, name);
+                                friends.save();
+                            } else {
+                                System.out.println(friends.getProperty(personId));
+                            }
+
+                        } catch (Exception e) {
+                            String personURL = "https://www.facebook.com/profile.php?id=" + person.substring(0, person.indexOf("@"));
+                            System.out.println(personURL);
+                        }
+                    }
+                    System.out.println("");
+
+                    System.out.println("Messages count: " + mes.size());
+                    i++;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 }
